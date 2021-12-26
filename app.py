7eb60ed58964bd358c8236f077c8881885a10546
mooyung <mooyung09@gmail.com>
2021-12-26 17:10:45 +0900

    memo 저장시 크롤링해서 데이터 추가

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import requests
+from bs4 import BeautifulSoup
 from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
 
@@ -31,9 +33,23 @@ def post_article():
     # ajax 사용해서 전송하는 data 정보는 request.form 변수에 저장됨
     # print(request.form.get('url_give'))
     # print(request.form.get('comment_give'))
+    url = request.form.get('url_give')
+    # 크롤링시 브라우저인냥 위장
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    crawling_data = requests.get(url, headers=headers)
+    soup = BeautifulSoup(crawling_data.text, 'html.parser')
+
+    og_img_url = soup.select_one('meta[property="og:image"]')['content']
+    og_title = soup.select_one('meta[property="og:title"]')['content']
+    og_description = soup.select_one('meta[property="og:description"]')['content']
+
     memo = {
     'url':request.form.get('url_give'),
     'comment':request.form.get('comment_give'),
+    'img_url': og_img_url,
+    'title': og_title,
+    'description': og_description,
     }
 
     # MongoDB저장
@@ -44,6 +60,7 @@ def post_article():
         'msg': '저장완료'
     }
     return jsonify(result)
+
 
 
 if __name__ == '__main__':
